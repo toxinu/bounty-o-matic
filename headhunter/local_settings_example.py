b7a8cc6a8e594dbda6c0bb7a0dd045db1f8632fb
Geoffrey Lehée <hello@socketubs.org>
2015-03-20 20:18:27 +0100

    Add cache file for example (nice for dev)

--- headhunter/local_settings_example.py
+++ headhunter/local_settings_example.py
@@ -5,3 +5,10 @@ from .settings import TEMPLATE_CONTEXT_PROCESSORS
 
 SOCIAL_AUTH_BATTLENET_OAUTH2_KEY = ''
 SOCIAL_AUTH_BATTLENET_OAUTH2_SECRET = ''
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/headhunter_cache',
+    }
+}
