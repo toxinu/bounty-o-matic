99f39b38241f3fb35e826b5b7720a5bd6d31393e
Geoffrey Lehée <hello@socketubs.org>
2015-03-20 21:00:00 +0100

    Remove useless routes

--- headhunter/urls.py
+++ headhunter/urls.py
@@ -13,17 +13,15 @@ urlpatterns = patterns(
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', accounts_views.HomeView.as_view(), name='home'),
     url(r'^logout/$', accounts_views.LogoutView.as_view(), name='logout'),
-    url(r'^bounties/$', bounties_views.BountyListView.as_view(), name='bounties-list'),
-    url(r'^bounties/add/$',
-        bounties_views.BountyCreateView.as_view(), name='bounties-add'),
-
+    # API
     url(r'^api/bounty',
         bounties_views.BountyListAPIView.as_view(), name='api-bounty-list'),
-    url(r'^api/regions.json',
+    url(r'^api/regions',
         battlenet_views.RegionsAPIView.as_view(), name='api-regions'),
-    url(r'^api/realms.json', battlenet_views.RealmsAPIView.as_view(), name='api-realms'),
-    url(r'^api/player-battletag.json',
+    url(r'^api/realms',
+        battlenet_views.RealmsAPIView.as_view(), name='api-realms'),
+    url(r'^api/player-battletag',
         battlenet_views.PlayerBattleTagAPIView.as_view(), name='api-player-battletag'),
-    url(r'^api/player-characters.json',
+    url(r'^api/player-characters',
         battlenet_views.PlayerCharactersAPIView.as_view(), name='api-player-characters'),
 )
