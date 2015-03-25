00a80ff95a65074caa6f4639f92f35ca46950de9
toxinu <toxinu@gmail.com>
2015-03-25 11:36:14 +0100

    Add cachalot to installed apps

--- headhunter/settings.py
+++ headhunter/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'memoize',
+    'cachalot',
     'sslserver',
     'social.apps.django_app.default',
     'headhunter.accounts',
