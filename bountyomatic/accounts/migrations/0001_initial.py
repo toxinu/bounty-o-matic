0af59f4a7efe14134426e0889c132e89ad709722
toxinu <toxinu@gmail.com>
2015-03-30 15:39:57 +0200

    Add migrations

---
+++ bountyomatic/accounts/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status', default=False)),
+                ('username', models.CharField(unique=True, verbose_name='username', help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')], max_length=30)),
+                ('first_name', models.CharField(blank=True, verbose_name='first name', max_length=30)),
+                ('last_name', models.CharField(blank=True, verbose_name='last name', max_length=30)),
+                ('email', models.EmailField(blank=True, verbose_name='email address', max_length=75)),
+                ('is_staff', models.BooleanField(help_text='Designates whether the user can log into this admin site.', verbose_name='staff status', default=False)),
+                ('is_active', models.BooleanField(help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active', default=True)),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('groups', models.ManyToManyField(blank=True, verbose_name='groups', related_name='user_set', to='auth.Group', related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.')),
+                ('user_permissions', models.ManyToManyField(blank=True, verbose_name='user permissions', related_name='user_set', to='auth.Permission', related_query_name='user', help_text='Specific permissions for this user.')),
+            ],
+            options={
+                'verbose_name_plural': 'users',
+                'verbose_name': 'user',
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
