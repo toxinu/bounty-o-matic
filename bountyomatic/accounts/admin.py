c334d3d8da8e8045975e1d133a2d622ea79d7679
toxinu <toxinu@gmail.com>
2015-03-30 15:39:35 +0200

    Update admin

--- bountyomatic/accounts/admin.py
+++ bountyomatic/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import User
+from ..battlenet.api import get_player_battletag
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'battletag', 'email', 'is_staff', )
+
+    def battletag(self, obj):
+        try:
+            return '<a href="%s" target="_blank">%s</a>' % (
+                reverse('admin:%s_%s_change' % (
+                    obj._meta.app_label, obj._meta.module_name),
+                    args=(obj.id,)),
+                get_player_battletag(obj))
+        except:
+            return '%s (%s)' % (obj, _("Battletag not found"))
+    battletag.allow_tags = True
+
+admin.site.register(User, CustomUserAdmin)
--- bountyomatic/bounties/admin.py
+++ bountyomatic/bounties/admin.py
@@ -1,7 +1,75 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Bounty
 from .models import Comment
 
-admin.site.register(Bounty)
-admin.site.register(Comment)
+from ..battlenet.api import get_player_battletag
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'battletag', 'character', 'bounty',
+        'added_date', 'is_hidden', 'external',)
+    list_filter = ('added_date', 'is_hidden', )
+
+    def battletag(self, obj):
+        return '<a href="%s" target="_blank">%s</a>' % (
+            reverse('admin:%s_%s_change' % (
+                    obj._meta.app_label, obj._meta.module_name),
+                    args=(obj.user.id,)),
+            get_player_battletag(obj.user))
+    battletag.allow_tags = True
+
+    def external(self, obj):
+        return '<a href="%s#comments" target="_blank">%s</a>' % (
+            reverse('bounty-detail', args=(obj.pk,)),
+            'View')
+    external.allow_tags = True
+    external.short_description = _("Context")
+
+    def character(self, obj):
+        return '<a href="%s" target="_blank">%s - %s</a>' % (
+            obj.character_armory,
+            obj.character_name,
+            obj.get_character_realm_display())
+    character.allow_tags = True
+
+
+class BountyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'battletag', 'status', 'region', 'source',
+        'destination', 'added_date', 'updated_date',
+        'comments_counter', )
+    list_filter = ('added_date', 'updated_date', 'is_private', 'status', )
+
+    def battletag(self, obj):
+        try:
+            return '<a href="%s" target="_blank">%s</a>' % (
+                reverse('admin:%s_%s_change' % (
+                    obj._meta.app_label, obj._meta.module_name),
+                    args=(obj.user.id,)),
+                get_player_battletag(obj.user))
+        except:
+            return '%s (%s)' % (obj.user, _("Battletag not found"))
+    battletag.allow_tags = True
+
+    def source(self, obj):
+        return '<a href="%s" target="_blank">%s - %s</a>' % (
+            obj.source_armory,
+            obj.source_character, obj.get_source_realm_display())
+    source.allow_tags = True
+
+    def destination(self, obj):
+        return '<a href="%s" target="_blank">%s - %s</a>' % (
+            obj.destination_armory,
+            obj.destination_character, obj.get_destination_realm_display())
+    destination.allow_tags = True
+
+    def comments_counter(self, obj):
+        return obj.comment_set.count()
+    comments_counter.short_description = _("Comments")
+
+admin.site.register(Bounty, BountyAdmin)
+admin.site.register(Comment, CommentAdmin)
