8915cf21b7a94d7b65c8b606d393fdadef479336
toxinu <toxinu@gmail.com>
2015-03-31 11:49:21 +0200

    Cache rendered image instead of thumbnail

--- bountyomatic/bounties/utils/export.py
+++ bountyomatic/bounties/utils/export.py
@@ -4,23 +4,21 @@ from weasyprint import HTML
 from django.template import loader
 from django.template import Context
 from django.contrib.staticfiles import finders
+from django.core.files.base import ContentFile
 
-from requests_cache.core import CachedSession
-
-
-def fetcher(url):
-    s = CachedSession(expire_after=60 * 60 * 24 * 1)
-    s.get(url)
-    resp = s.get(url)
-    return {
-        'string': resp.content,
-        'mime_type': resp.headers.get('content-type'),
-        'encoding': resp.encoding}
+from ..models import BountyImage
 
 
 def render_bounty(bounty):
-    t = loader.get_template('bounties/export.html')
-    css_path = finders.find('bountyomatic/css/export.css')
-
-    html = HTML(string=t.render(Context({'bounty': bounty})), url_fetcher=fetcher)
-    return html.write_png(stylesheets=[CSS(filename=css_path)])
+    bounty_image, _ = BountyImage.objects.get_or_create(bounty=bounty)
+    if bounty_image.is_expired():
+        t = loader.get_template('bounties/export.html')
+        css_path = finders.find('bountyomatic/css/export.css')
+        html = HTML(string=t.render(Context({'bounty': bounty})))
+        raw_image = html.write_png(stylesheets=[CSS(filename=css_path)])
+        image_content = ContentFile(raw_image)
+        image_content.name = "bounty_%s.jpg" % bounty.pk
+        bounty_image.image = image_content
+        bounty_image.save()
+        return raw_image
+    return bounty_image.image.read(-1)
