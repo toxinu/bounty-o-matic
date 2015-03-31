b8a645b1c9184e6834c0c165d0ba366863566024
toxinu <toxinu@gmail.com>
2015-03-31 20:48:34 +0200

    Add bountyomatic js

---
+++ static/bountyomatic/js/bountyomatic.js
@@ -0,0 +1,113 @@
+function loadLanguageSwitcher() {
+	// Language switcher
+  $("ul#language-switcher li a").click(function() {
+    var lang = $(this).attr('value');
+    var $form = $('<form style="display:none;" action="/i18n/setlang/" method="POST">' +
+      "{% csrf_token %}" + 
+      '<input type="hidden" name="language" value="' + lang + '">' +
+      '</form>');
+    $form.appendTo("body").submit();
+  });
+};
+function getPlayerBattletag(callback) {
+	$.ajax({
+    url: "/api/player-battletag",
+    dataType: 'json',
+    success: function(data) {
+      if (callback)
+        callback(data)
+      unsetProgressBar(data.battletag);
+    },
+    error: function(xhr, status, err) {
+      console.error("/api/player-battletag", status, err.toString());
+    },
+  });
+};
+function setProgressBar(text) {
+  var battletag = $("div.battletag");
+  battletag.text(text);
+  battletag.addClass("active");
+  battletag.addClass("progress-bar-striped");
+  battletag.addClass("progress-bar-warning");
+};
+function unsetProgressBar(text) {
+  var battletag = $("div.battletag");
+  battletag.text(text);
+  battletag.removeClass("active");
+  battletag.removeClass("progress-bar-striped");
+  battletag.addClass("progress-bar");
+  battletag.removeClass("progress-bar-warning");
+};
+function getUrlParameter(sParam)
+{
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1] || true;
+    }
+  }
+};
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function sameOrigin(url) {
+  // test that a given url is a same-origin URL
+  // url could be relative or scheme relative or absolute
+  var host = document.location.host; // host + port
+  var protocol = document.location.protocol;
+  var sr_origin = '//' + host;
+  var origin = protocol + sr_origin;
+  // Allow absolute or scheme relative URLs to same origin
+  return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+    (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+    // or any other URL that isn't scheme relative or absolute i.e relative.
+    !(/^(\/\/|http:|https:).*/.test(url));
+}
+
+$(function() {
+    $.xhrPool = [];
+    $.xhrPool.abortAll = function() {
+        $(this).each(function(i, jqXHR) {   //  cycle through list of recorded connection
+            jqXHR.abort();  //  aborts connection
+            $.xhrPool.splice(i, 1); //  removes from list by index
+        });
+    }
+    $.ajaxSetup({
+        beforeSend: function(jqXHR, settings) {
+          $.xhrPool.push(jqXHR);
+          if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+            // Send the token to same-origin, relative URLs only.
+            // Send the token only if the method warrants CSRF protection
+            // Using the CSRFToken value acquired earlier
+            var csrftoken = getCookie('csrftoken');
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+          }
+        },
+        complete: function(jqXHR) {
+            var i = $.xhrPool.indexOf(jqXHR);   //  get index for current connection completed
+            if (i > -1) $.xhrPool.splice(i, 1); //  removes from list by index
+        }
+    });
+
+    $(window).on('location'), function(e) {
+      $.xhrPool.abortAll();
+    };
+})
