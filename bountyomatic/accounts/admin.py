4208fd2130626105c24525178cc71b67c17addc0
toxinu <toxinu@gmail.com>
2015-04-04 21:11:53 +0200

    Refresh battlenet user data from admin

--- bountyomatic/accounts/admin.py
+++ bountyomatic/accounts/admin.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from .models import User
+from ..battlenet.api import refresh_player_cache
 from ..battlenet.api import get_player_battletag
 
 
@@ -12,16 +13,22 @@ class CustomUserAdmin(UserAdmin):
         'username', 'battletag', 'email',
         'date_joined', 'last_login', 'is_staff',)
     ordering = ['-date_joined']
+    actions = ['refresh_user_data']
 
     def battletag(self, obj):
         try:
             return '<a href="%s" target="_blank">%s</a>' % (
                 reverse('admin:%s_%s_change' % (
-                    obj._meta.app_label, obj._meta.module_name),
+                    obj._meta.app_label, obj._meta.model_name),
                     args=(obj.id,)),
                 get_player_battletag(obj))
         except:
             return '%s (%s)' % (obj, _("Battletag not found"))
     battletag.allow_tags = True
+
+    def refresh_user_data(self, request, queryset):
+        for user in queryset.all():
+            refresh_player_cache(user)
+    refresh_user_data.short_description = _("Refresh user BattleNet data")
 
 admin.site.register(User, CustomUserAdmin)
