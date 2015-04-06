90112b77464574a9de8dcbf1901fbfe8f5be1144
toxinu <toxinu@gmail.com>
2015-04-06 11:57:17 +0200

    Clean the js

--- static/bountyomatic/js/bountyomatic.js
+++ static/bountyomatic/js/bountyomatic.js
@@ -1,17 +1,6 @@
-function getPlayerBattletag(callback) {
-	$.ajax({
-    url: "/api/player-battletag",
-    dataType: 'json',
-    success: function(data) {
-      if (callback)
-        callback(data)
-      unsetProgressBar(data.battletag);
-    },
-    error: function(xhr, status, err) {
-      console.error("/api/player-battletag", status, err.toString());
-    },
-  });
-};
+//
+// Utils
+//
 function setProgressBar(text) {
   var battletag = $("div.battletag");
   battletag.text(text);
@@ -27,59 +16,59 @@ function unsetProgressBar(text) {
   battletag.addClass("progress-bar");
   battletag.removeClass("progress-bar-warning");
 };
-function getUrlParameter(sParam) {
-  var sPageURL = window.location.search.substring(1);
-  var sURLVariables = sPageURL.split('&');
-  for (var i = 0; i < sURLVariables.length; i++) {
-    var sParameterName = sURLVariables[i].split('=');
-    if (sParameterName[0] == sParam) {
-      return sParameterName[1] || true;
+//
+// API
+//
+function getPlayerBattletag(callback) {
+	$.ajax({
+    url: "/api/player-battletag",
+    dataType: 'json',
+    success: function(data) {
+      if (callback)
+        callback(data)
+      unsetProgressBar(data.battletag);
+    },
+    error: function(xhr, status, err) {
+      console.error("/api/player-battletag", status, err.toString());
+    },
+  });
+};
+function getRegions(callback) {
+  $.ajax({
+    url: "/api/regions",
+    dataType: 'json',
+    success: function(data) {
+      if (callback)
+        callback(data);
+    },
+    error: function(xhr, status, err) {
+      console.error("/api/regions", status, err.toString());
     }
-  }
-};
-function csrfSafeMethod(method) {
-  // these HTTP methods do not require CSRF protection
-  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-};
-function sameOrigin(url) {
-  // test that a given url is a same-origin URL
-  // url could be relative or scheme relative or absolute
-  var host = document.location.host; // host + port
-  var protocol = document.location.protocol;
-  var sr_origin = '//' + host;
-  var origin = protocol + sr_origin;
-  // Allow absolute or scheme relative URLs to same origin
-  return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-    (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-    // or any other URL that isn't scheme relative or absolute i.e relative.
-    !(/^(\/\/|http:|https:).*/.test(url));
+  }); 
 }
-$(function() {
-    $.xhrPool = [];
-    $.xhrPool.abortAll = function() {
-        $(this).each(function(i, jqXHR) {   //  cycle through list of recorded connection
-            jqXHR.abort();  //  aborts connection
-            $.xhrPool.splice(i, 1); //  removes from list by index
-        });
+function getRealms(region, callback) {
+  $.ajax({
+    url: "/api/realms?region=" + region,
+    dataType: 'json',
+    success: function(data) {
+      if (callback)
+        callback(data);
+    },
+    error: function(xhr, status, err) {
+      console.error("/api/realms?region=" + region, status, err.toString());
     }
-    $.ajaxSetup({
-        beforeSend: function(jqXHR, settings) {
-          $.xhrPool.push(jqXHR);
-          if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-            // Send the token to same-origin, relative URLs only.
-            // Send the token only if the method warrants CSRF protection
-            // Using the CSRFToken value acquired earlier
-            var csrftoken = $.cookie('csrftoken');
-            jqXHR.setRequestHeader("X-CSRFToken", csrftoken);
-          }
-        },
-        complete: function(jqXHR) {
-            var i = $.xhrPool.indexOf(jqXHR);   //  get index for current connection completed
-            if (i > -1) $.xhrPool.splice(i, 1); //  removes from list by index
-        }
-    });
-
-    $(window).on('location'), function(e) {
-      $.xhrPool.abortAll();
-    };
-})
+  }); 
+};
+function getPlayerCharacters(callback) {
+  $.ajax({
+    url: "/api/player-characters",
+    dataType: 'json',
+    success: function(data) {
+      if (callback)
+        callback(data);
+    },
+    error: function(xhr, status, err) {
+      console.error("/api/player-characters", status, err.toString());
+    }
+  });  
+};
---
+++ static/bountyomatic/js/utils.js
@@ -0,0 +1,56 @@
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1] || true;
+    }
+  }
+};
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+};
+function sameOrigin(url) {
+  // test that a given url is a same-origin URL
+  // url could be relative or scheme relative or absolute
+  var host = document.location.host; // host + port
+  var protocol = document.location.protocol;
+  var sr_origin = '//' + host;
+  var origin = protocol + sr_origin;
+  // Allow absolute or scheme relative URLs to same origin
+  return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+    (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+    // or any other URL that isn't scheme relative or absolute i.e relative.
+    !(/^(\/\/|http:|https:).*/.test(url));
+}
+$(function() {
+  $.xhrPool = [];
+  $.xhrPool.abortAll = function() {
+    $(this).each(function(i, jqXHR) {   //  cycle through list of recorded connection
+      jqXHR.abort();  //  aborts connection
+      $.xhrPool.splice(i, 1); //  removes from list by index
+    });
+  }
+  $.ajaxSetup({
+    beforeSend: function(jqXHR, settings) {
+      $.xhrPool.push(jqXHR);
+      if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+        // Send the token to same-origin, relative URLs only.
+        // Send the token only if the method warrants CSRF protection
+        // Using the CSRFToken value acquired earlier
+        var csrftoken = $.cookie('csrftoken');
+        jqXHR.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    },
+    complete: function(jqXHR) {
+      var i = $.xhrPool.indexOf(jqXHR);   //  get index for current connection completed
+      if (i > -1) $.xhrPool.splice(i, 1); //  removes from list by index
+  }
+  });
+
+  $(window).on('location'), function(e) {
+    $.xhrPool.abortAll();
+  };
+})
