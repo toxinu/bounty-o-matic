578ab3e872697672d2e4660f7db0817a7df4fb58
toxinu <toxinu@gmail.com>
2015-04-11 18:58:09 +0200

    Add ban user action in admin

--- bountyomatic/accounts/admin.py
+++ bountyomatic/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.core.urlresolvers import reverse
+from django.contrib.sessions.models import Session
 from django.utils.translation import ugettext_lazy as _
 
 from .models import User
@@ -12,8 +13,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = (
         'username', 'battletag', 'email',
         'date_joined', 'last_login', 'is_staff',)
-    ordering = ['-date_joined']
-    actions = ['refresh_user_data']
+    ordering = ('-date_joined', )
+    actions = ('refresh_user_data', 'ban_user', )
 
     def battletag(self, obj):
         try:
@@ -30,5 +31,14 @@ class CustomUserAdmin(UserAdmin):
         for user in queryset.all():
             refresh_player_cache(user)
     refresh_user_data.short_description = _("Refresh user BattleNet data")
+
+    def ban_user(self, request, queryset):
+        for user in queryset.all().only('id'):
+            for s in Session.objects.all():
+                if s.get_decoded().get('_auth_user_id') == user.id:
+                    s.delete()
+            user.is_active = False
+            user.save()
+    ban_user.short_description = _("Ban user")
 
 admin.site.register(User, CustomUserAdmin)
