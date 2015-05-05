8c631b5fbda7a9e6bff3e80e86b1697204c0afbd
toxinu <toxinu@gmail.com>
2015-05-05 15:12:31 +0200

    Add new battletag field for User (in order to detect empty battletag)

--- bountyomatic/accounts/models.py
+++ bountyomatic/accounts/models.py
@@ -1,6 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Empty User overwrite for later maybe
 class User(AbstractUser):
-    pass
+    battletag = models.CharField(max_length=20, null=True, blank=True, unique=True)
+
+    def clean(self):
+        from ..battlenet.api import get_player_battletag
+
+        super().clean()
+        self.battletag = get_player_battletag(self)
