14711cc5a1b53c31267bcf5ca55904f6aed96c6d
toxinu <toxinu@gmail.com>
2015-05-07 14:49:54 +0200

    Update migration

--- bountyomatic/bounties/migrations/0018_auto_20150507_1208.py
+++ bountyomatic/bounties/migrations/0018_auto_20150507_1208.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='bounty',
             name='slug',
-            field=models.UUIDField(unique=True, default=uuid.uuid4),
+            field=models.UUIDField(default=uuid.uuid4),
         ),
     ]
